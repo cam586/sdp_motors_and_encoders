8e7713f23f4691084c9cb6ab585239d7ef2d1394
cam586 <c.mcgreavy@hotmail.com>
2020-01-09 19:53:46 +0000

    added load time to SMBus init

--- motors.py
+++ motors.py
@@ -5,7 +5,10 @@ from datetime import datetime
 
 class Motors(object):
 	def __init__(self):
+		print "Starting SMBus . . ."
 		self.bus = smbus.SMBus(1)
+		sleep(2)
+		print "SMBus Started."
 		self.mc = MotorControl()
 		self.encoder_address = 0x05
 		self.encoder_register = 0x0
